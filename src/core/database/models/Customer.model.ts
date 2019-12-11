f0a825064dc9f8a19c2b88b1705afbab84ee228c
mateuszskibickiplt <mateusz.skibicki@prettylittlething.com>
2019-12-11 12:20:05 +0000

    models

--- src/core/database/models/Customer.model.ts
+++ src/core/database/models/Customer.model.ts
@@ -1,9 +1,25 @@
-import { attribute, table } from "@aws/dynamodb-data-mapper-annotations";
+import {
+  attribute,
+  table,
+  rangeKey
+} from "@aws/dynamodb-data-mapper-annotations";
 import { DatabaseEnum } from "../";
 import { BaseModel } from "./BaseModel.model";
 
 @table(DatabaseEnum.Table)
 export class Customer extends BaseModel<Customer> {
+  /**
+   * @var {Date} createdAt
+   */
+  @rangeKey({ defaultProvider: () => new Date() })
+  createdAt: Date;
+
+  /**
+   * @var {Date} updatedAt
+   */
+  @attribute({ defaultProvider: () => new Date() })
+  updatedAt: Date;
+
   /**
    * @var {string} firstName
    */
